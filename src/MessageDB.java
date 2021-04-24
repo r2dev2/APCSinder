7b5e3b147f14115f730c684ec0110b7d8de2de8b
r2dev2 <ronak.badhe@gmail.com>
2021-04-24 15:53:33 -0700

    javadoc for databases

--- src/MessageDB.java
+++ src/MessageDB.java
@@ -1,28 +1,58 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * A persistent database of messages to be used in the backend.
+ *
+ * @author Ronak Badhe
+ * @version Sat Apr 24 15:48:52 PDT 2021
+ */
 public class MessageDB
 {
     private String filename;
     private HashMap<Match, ArrayList<Message>> messages;
+    private UserDB users;
 
-    public MessageDB(String filename)
+    /**
+     * Constructor.
+     *
+     * @param filename the filename to persist this messagedb to
+     * @param users the userdb to use for authentication
+     */
+    public MessageDB(String filename, UserDB users)
     {
         this.filename = filename;
+        this.users = users;
     }
 
+    /**
+     * Add a message to the databse.
+     *
+     * @param msg the message to add
+     */
     public void add(Message msg)
     {
         // TODO
     }
 
-    public ArrayList<Message> get(User user, String token)
+    /**
+     * Get the messages for a particlar user pair.
+     *
+     * @param match the user pair
+     * @param token the login token of one of the users
+     */
+    public ArrayList<Message> get(Match match, String token)
     {
         // TODO
         return null;
     }
 
     private void load()
+    {
+        // TODO
+    }
+
+    private void save()
     {
         // TODO
     }
--- src/UserDB.java
+++ src/UserDB.java
@@ -1,6 +1,9 @@
 import java.util.HashMap;
 import java.util.HashSet;
 
+/**
+ * A persistent database for user authentication.
+ */
 public class UserDB
 {
     private String filename;
@@ -9,23 +12,45 @@ public class UserDB
     // Map user -> token
     private HashMap<User, String> loggedIn;
 
+    /**
+     * Constructor.
+     *
+     * @param filename the filename to persist to
+     */
     public UserDB(String filename)
     {
         this.filename = filename;
     }
 
+    /**
+     * Logs in a user.
+     *
+     * @param user the user to log in
+     * @param password the user's password
+     */
     public LoginResult login(User user, String password)
     {
         // TODO
         return null;
     }
 
+    /**
+     * Checks if the user is logged in with the token.
+     *
+     * @param user the user
+     * @param token the token
+     */
     public boolean authenticate(User user, String token)
     {
         return false;
     }
 
     private void load()
+    {
+        // TODO
+    }
+
+    private void save()
     {
         // TODO
     }
