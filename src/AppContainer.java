60efbe5cd90e461807946a2b5bc7d739c5601ca1
JustinChang04 <chang.justin0106@gmail.com>
2021-04-24 13:46:34 -0700

    Add the AppContainer class to be implemented.

---
+++ src/AppContainer.java
@@ -0,0 +1,12 @@
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ *
+ *  @author Justin Chang
+ *  @version Apr 24, 2021
+ */
+public class AppContainer
+{
+    //TODO add the JPanel for the ChatUI and Matching UI.
+}
