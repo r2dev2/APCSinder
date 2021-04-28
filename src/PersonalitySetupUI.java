37010ea88ec4dca3aadc61dbf0a98f4dbe660613
Kevin Li <kevinli.kl@outlook.com>
2021-04-28 14:26:49 -0700

    Javadocs for PersonalitySetupUI

--- src/PersonalitySetupUI.java
+++ src/PersonalitySetupUI.java
@@ -6,9 +6,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- *  Write a one-sentence summary of your class here.
- *  Follow it with additional details about its purpose, what abstraction
- *  it represents, and how to use it.
+ *  The UI end for setting up a user's personality traits.
+ *  Guides the user through the personality questions. Has a PersonalityTest
+ *  field to retrieve and store questions and answers. Is used by AppContainer.
  *
  *  @author Kevin Li
  *  @version Apr 25, 2021
@@ -19,6 +19,9 @@ public class PersonalitySetupUI extends JPanel
     //~ Fields ................................................................
     private String username;
     private PersonalityTest test;
+    /**
+     * Used to set up the formatting for each component of the JPanel.
+     */
     GridBagConstraints constraint = new GridBagConstraints();
     private JLabel welcome;
     private JLabel question;
@@ -28,6 +31,14 @@ public class PersonalitySetupUI extends JPanel
     private AppContainer container;
 
     //~ Constructors ..........................................................
+    /**
+     * Create a new PersonalitySetupUI object.
+     * Sets up instance variables and basic layout accordingly, then starts up the
+     * rest of the UI.
+     * @param name  User name
+     * @param visible   true if UI is visible, false otherwise
+     * @param a  a reference to the AppContainer that uses this class.
+     */
     public PersonalitySetupUI(String name, boolean visible, AppContainer a) {
         username = name;
         test = new PersonalityTest(name);
@@ -35,18 +46,6 @@ public class PersonalitySetupUI extends JPanel
 
         setLayout(new GridBagLayout());
         setFeel();
-
-        welcome = new JLabel("<html><p>Welcome to APCSinder! Please take a quick personality"
-            + " test before you can start matching.\nWe will ask you a series of 8 questions where"
-            + " you will rate yourself on a scale of 0 to 10 on how close you identify with a"
-            + " particular viewpoint. Please be as truthful as possible.</p></html>");
-        constraint.fill = GridBagConstraints.HORIZONTAL;
-        constraint.gridx = 0;
-        constraint.gridy = 0;
-        constraint.insets = new Insets(10, 10, 10, 10);
-        constraint.weightx = 0.5;
-        constraint.gridwidth = 2;
-        add(welcome, constraint);
 
         setVisible(visible);
         start();
@@ -70,7 +69,25 @@ public class PersonalitySetupUI extends JPanel
         }
     }
     //~Public  Methods ........................................................
+    /**
+     * Sets up the main components of the UI: a welcome message at the top, then the
+     * question on the left, a slider on the right, a label with the current response on the
+     * bottom left, and the next button on the bottom right.
+     *
+     */
     public void start() {
+        welcome = new JLabel("<html><p>Welcome to APCSinder! Please take a quick personality"
+            + " test before you can start matching.\nWe will ask you a series of 8 questions where"
+            + " you will rate yourself on a scale of 0 to 10 on how close you identify with a"
+            + " particular viewpoint. Please be as truthful as possible.</p></html>");
+        constraint.fill = GridBagConstraints.HORIZONTAL;
+        constraint.gridx = 0;
+        constraint.gridy = 0;
+        constraint.insets = new Insets(10, 10, 10, 10);
+        constraint.weightx = 0.5;
+        constraint.gridwidth = 2;
+        add(welcome, constraint);
+
         question = new JLabel("<html><p>" + test.getQuestion() + "</p></html>");
         constraint.fill = GridBagConstraints.HORIZONTAL;
         constraint.gridx = 0;
@@ -116,6 +133,9 @@ public class PersonalitySetupUI extends JPanel
         add(nextButton, constraint);
     }
 
+    /**
+     * Refreshes the contents on the screen for the next question.
+     */
     public void next() {
         test.answerQuestion(slider.getValue());
         test.nextQuestion();
@@ -128,6 +148,10 @@ public class PersonalitySetupUI extends JPanel
         }
     }
 
+    /**
+     * Closes up the personality setup and proceeds with matching (which is specified by
+     * another class).
+     */
     public void finish() {
         container.personalityToMatching();
     }
