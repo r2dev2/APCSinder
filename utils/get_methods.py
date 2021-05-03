11e7785a20722be147ef9e98b85ef957076a95d0
r2dev2 <ronak.badhe@gmail.com>
2021-05-03 08:26:50 -0700

    made a get methods script for class diagram

---
+++ utils/get_methods.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import re
+import sys
+import os
+
+def do_stuff(fn):
+    with open(fn, 'r') as fin:
+        file = fin.read()
+
+    method_re = re.compile(
+        r"(public [^\W]+ [^\W]+[^\n]+)"
+    )
+
+    print("Properties and methods of", fn)
+    print(*re.findall(method_re, file), sep='\n')
+    print()
+
+list(map(do_stuff, sys.argv[1:]))
