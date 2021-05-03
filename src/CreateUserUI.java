544cdfc5d9eadd6f459bc97197fa3232dcbcf243
Kevin Li <kevinli.kl@outlook.com>
2021-05-03 09:22:22 -0700

    User setup UI at the beginning

--- src/CreateUserUI.java
+++ src/CreateUserUI.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionListener;
 /**
  *  Write a one-sentence summary of your class here
  *  Follow it with additional details about its purpose, what abstraction
- *  it represents, and how to use it.
+ *  it represents, and how to use it
  *
  *  @author Kevin Li
  *  @version May 3, 2021
