8c1a7291ac98f60dafbc201a133ade7da2c15e49
Kevin Li <kevinli.kl@outlook.com>
2021-05-04 20:57:18 -0700

    minor foundational work for matching UI

--- src/MatchingUI.java
+++ src/MatchingUI.java
@@ -11,13 +11,44 @@ import java.awt.*;
  */
 public class MatchingUI extends JPanel
 {
+    // Fields ................................................................
+    private String username;
+    private PersonalityTest test;
     private Network network;
+    /**
+     * Used to set up the formatting for each component of the JPanel.
+     */
+    private GridBagConstraints constraint = new GridBagConstraints();
+    private JLabel welcome;
+    private JLabel question;
+    private JSlider slider;
+    private JLabel answerLabel;
+    private JButton nextButton;
+    private AppContainer container;
 
     /**
      * Create a new MatchingUI object.
+     * @param name User name
      * @param network the network to connect to
      */
-    public MatchingUI(Network network) { //add other stuff as needed
+    public MatchingUI(String name, Network network) { //add other stuff as needed
+        username = name;
         this.network = network;
+        setLayout(new GridBagLayout());
+        setFeel();
+    }
+
+    /**
+     * Sets the feel of the UI.
+     */
+    private void setFeel()
+    {
+        try {
+            UIManager.setLookAndFeel (
+                    "javax.swing.plaf.nimbus.NimbusLookAndFeel"
+            );
+        } catch (Exception e) {
+            System.out.println("This is never supposed to get called");
+        }
     }
 }
